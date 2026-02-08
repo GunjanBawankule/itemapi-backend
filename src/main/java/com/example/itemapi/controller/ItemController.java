bcf1a3ee95b6bd938639425615b7771bcf55fa09
GunjanBawankule <gunjanbawnkule199@gmail.com>
2026-02-08 14:43:48 +0530

    Initial commit

--- src/main/java/com/example/itemapi/controller/ItemController.java
+++ src/main/java/com/example/itemapi/controller/ItemController.java
@@ -5,8 +5,7 @@ import com.example.itemapi.model.Item;
 import com.example.itemapi.service.ItemService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
-
-        import java.util.List;
+import java.util.List;
 
 @RestController
 @RequestMapping("/items")
@@ -17,6 +16,9 @@ public class ItemController {
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
+
+
+
 
     @GetMapping
     public List<Item> getItems() {
@@ -31,6 +33,11 @@ public class ItemController {
     @GetMapping("/{id}")
     public Item getItemById(@PathVariable Long id) {
         return itemService.getItemById(id);
+    }
+
+    @GetMapping("/index")
+    public String showWelcomeMessage() {
+        return "Gunjan";
     }
 
 }
